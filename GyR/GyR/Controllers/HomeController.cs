0166ba7455cc7d46393200dc886c7056ca137df1
Juank37 <138148043+JC37D3Y@users.noreply.github.com>
2024-11-11 09:43:03 -0600

    .

--- GyR/GyR/Controllers/HomeController.cs
+++ GyR/GyR/Controllers/HomeController.cs
@@ -26,5 +26,14 @@ namespace GyR.Controllers
 
             return View();
         }
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Price { get; set; }
+            public string ImageUrl { get; set; } // URL de la imagen del producto
+        }
+
     }
 }
